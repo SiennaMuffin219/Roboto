25ce45b298ae2093459218a119d4f943d8b0979e
Elyspio <jona.guich@hotmail.fr>
2018-01-12 18:50:43 +0100

    index.php : Table Done & Css

--- web/assets/php/index.php
+++ web/assets/php/index.php
@@ -4,16 +4,35 @@
 <head>
     <meta charset="utf-8">
     <title>Roboto</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Roboto</h1>
-    <p>
-        <?php
-        $dataFile = fopen('../data/speed.txt', 'r+');
-        $dataString = fgets($dataFile);
-        echo 'Le text : '. $dataString;
-        ?>
 
-    </p>
+        <table>
+        <tr>
+            <th>Vitesse gauche : </th>
+            <td>
+                <?php
+                $data = file("../data/speed.txt");
+                echo $data[1];
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <th>Vitesse droite : </th>
+            <td>
+                <?php
+                $data = file("../data/speed.txt");
+                echo  $data[2];
+                ?>
+            </td>
+        </tr>
+
+
+        </table>
+
+
 </body>
 </html>
