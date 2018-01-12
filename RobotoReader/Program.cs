0487b1ad8ab7931a364f4b58a95780d172e04362
Olivier Pautrat <olivier.pautrat@laposte.net>
2018-01-12 15:14:30 +0100

    First commit

---
+++ RobotoReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RobotoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            Stopwatch stopwatch = new Stopwatch();
+            string result = "";
+            while (true)
+            {
+                stopwatch.Restart();
+                try
+                {
+                    result = client.DownloadString($"http://roboto.elyspioweb.xyz/speed.txt");
+                } catch { }
+                stopwatch.Stop();
+                Console.Clear();
+                Console.WriteLine(result);
+                Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
