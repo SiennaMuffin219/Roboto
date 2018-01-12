c0a667e66ffccd69a9d4f76c83aed9bc25cae331
Elyspio <jona.guich@hotmail.fr>
2018-01-12 19:48:37 +0100

    Web : Syncro ftp

--- web/assets/php/index.php
+++ web/assets/php/index.php
@@ -3,36 +3,52 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <!-- <meta http-equiv="refresh" content="0.5" >  -->
     <title>Roboto</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+
     <h1>Roboto</h1>
-
-        <table>
+    <h2>Commands</h2>
+    <table>
         <tr>
-            <th>Vitesse gauche : </th>
+            <th>Left Speed</th>
             <td>
-                <?php
-                $data = file("../data/speed.txt");
-                echo $data[1];
-                ?>
+                <?php $data = file("../data/speed.txt"); echo  $data[1]; ?>
             </td>
         </tr>
 
         <tr>
-            <th>Vitesse droite : </th>
+            <th>Right Speed</th>
             <td>
-                <?php
-                $data = file("../data/speed.txt");
-                echo  $data[2];
-                ?>
+                <?php $data = file("../data/speed.txt"); echo  $data[2]; ?>
             </td>
         </tr>
+    </table>
 
+    <h2>Sensors</h2>
+    <table>
+        <tr>
+            <th>Contact</th>
+            <td></td>
+        </tr>
 
-        </table>
+        <tr>
+            <th></th>
+            <td></td>
+        </tr>
 
+        <tr>
+            <th></th>
+            <td></td>
+        </tr>
 
-</body>
-</html>
+        <tr>
+            <th></th>
+            <td></td>
+        </tr>
+            </table>
+
+        </body>
+        </html>
